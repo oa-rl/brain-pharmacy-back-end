32d8c7d4d7f9f3c4834374e2f9b3feb92df1e7b7
oarleon <oareyes@bi.com.gt>
2022-09-13 15:04:49 -0600

    :construction:

---
+++ core/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities
+{
+    [Table(name: "core_user")]
+    public class UserEntity: BaseEntity
+    {
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public int ProfileId { get; set; }
+        public ProfileEntity? Profile { get; set; }
+    }
+}
---
+++ core/Specifications/UserSpecParams.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Specifications
+{
+    public class UserSpecParams: BaseSpecParams
+    {
+        public int? id { get; set; }
+    }
+}
